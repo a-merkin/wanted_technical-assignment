8208415aedbb34262f42935eefc762efae07caf7
Arsen Merkin <a.merkin@iva.ru>
2024-07-08 01:55:06 +0500

    implemented logs logic

---
+++ src/types/logTypes.ts
@@ -0,0 +1,6 @@
+export interface Log {
+  timestamp: string
+  level: string
+  message: string
+  source: string
+}
--- src/views/BasePage.vue
+++ src/views/BasePage.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
-
+    <LogCard v-for="log in logs" v-bind="log"/>
   </div>
 </template>
 
 <script setup lang="ts">
 import wampService from '../websocket/wampService';
 import subscriptionService from '../api/services/subscriptionService';
+import LogCard from '../components/LogCard.vue';
+import type { Log } from '../types/logTypes';
+import { ref, onMounted } from 'vue';
 
 const socket = wampService
 socket.connect()
-subscriptionService.getLogsList()
+
+const logs = ref<Log[]>([])
+
+const getData = () => {
+  subscriptionService.getLogsList().then((response) => {
+    logs.value = response
+  })
+}
+
+onMounted(() => {
+  getData()
+})
 </script>
 
 <style scoped>
